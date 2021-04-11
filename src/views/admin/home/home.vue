81d4979ab3784a3f0be2b1afd698e42db0cebd04
zhangchao <1592727414@qq.com>
2021-04-11 21:03:11 +0800

    2021.4.11

--- src/views/admin/home/home.vue
+++ src/views/admin/home/home.vue
@@ -42,7 +42,7 @@
         </div>
       </el-header>
       <el-container>
-        <el-aside class="aside">
+        <el-aside class="aside" width="230px">
           <!--导航菜单  default-active="1-1"-->
           <el-menu class="el-menu-vertical-demo"
                    background-color="#4b5f6e"
@@ -58,13 +58,11 @@
               <el-menu-item index="1-2"
                             >菜单管理</el-menu-item>
             </el-submenu>
-            <el-menu-item index="2"
-                          disabled>
+            <el-menu-item index="2">
               <i class="el-icon-magic-stick"></i>
               <span slot="title">导航一</span>
             </el-menu-item>
-            <el-menu-item index="3"
-                          disabled>
+            <el-menu-item index="3">
               <i class="el-icon-reading"></i>
               <span slot="title">导航二</span>
             </el-menu-item>
